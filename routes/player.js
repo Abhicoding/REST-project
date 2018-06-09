8f6701d358997a262661d686054fe87c87699836
Abhicoding <shinganeabhishek@gmail.com>
2018-06-09 15:06:26 +0530

    tested delete and changed the verb

--- routes/player.js
+++ routes/player.js
@@ -4,7 +4,7 @@ const playerCtrl = require('../controller/player')
 
 router.get('/details/:id', playerCtrl.getPlayer)
 router.post('/details', playerCtrl.insertPlayer)
-router.post('/details/:id', playerCtrl.removePlayer)
+router.delete('/details/:id', playerCtrl.removePlayer)
 
 router.get('/starwars/:peopleNum', playerCtrl.swapi)
 
