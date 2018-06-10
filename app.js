5bfb4ee50e79daa584400826c3d078b00e53fe75
Abhicoding <shinganeabhishek@gmail.com>
2018-06-11 01:22:05 +0530

    added logging, route error handling & CORS handling

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 const playerRoutes = require('./routes/player')
 const teamRoutes = require('./routes/team')
 
 app.use(bodyParser.json())
+app.use(morgan('dev'))
 
 app.use('/api/player', playerRoutes)
 app.use('/api/team', teamRoutes)
 
 app.use((req, res, next) => {
-  res.status(200).json({
-    message: 'It works!'
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin X-Requested-With Content-Type, Accept, Authorization')
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+    return res.status(200).json({})
+  }
+  next()
+})
+
+app.use((req, res, next) => {
+  const error = new Error('Not found')
+  error.status(404)
+  next(error)
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  res.json({
+    error: {
+      message: error.message
+    }
   })
 })
 
--- controller/team.js
+++ controller/team.js
@@ -7,18 +7,18 @@ exports.insertTeam = (req, res) => {
 }
 
 exports.getTeam = (req, res) => {
-  let id = req.params.id
+  let id = parseInt(req.params.id)
   teamModel.get(id)
     .then(result => res.status(200).json(result))
 }
 
 exports.editTeam = (req, res) => {
-  let id = req.params.id
+  let id = parseInt(req.params.id)
   let obj = req.body
   teamModel.edit(id, obj).then(result => res.json(200).json(result))
 }
 
 exports.removeTeam = (req, res) => {
-  let id = req.params.id
+  let id = parseInt(req.params.id)
   teamModel.remove(id).then(result => res.status(200).send(result))
 }
